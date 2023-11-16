dc731d171882f76e963a76156b592e55634c288d
clebin <cleberson.memorialeite@gmail.com>
2023-11-16 09:55:49 +0000

    teste

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function addVal(i){
     document.getElementById("resultados").value = valores;
 
 }
+function operation(x){
+
+    valores+=x;
+        if(x=="C"){
+            valores="";
+    }
+    document.getElementById("resultado").value = valores 
+
+
+}
+
+function calculate(){
+
+    try{
+   valores= eval(valores);
+   document.getElementById("resultado").value = valores 
+
+    }
+    catch(e){
+alert("reveja sua operação")
+    }
+
+} 
